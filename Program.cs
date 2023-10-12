c41344424b878c61b9602d2906e608e9d0e9e7f3
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-12 09:19:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsMale = true;
+            if (IsMale) 
+            {
+                Console.WriteLine(" You are male");
+            }
+            else 
+            {
+                Console.WriteLine("You are female");
+            }
+            bool IsTall = false;
+            if (IsTall && IsMale) 
+            {
+                Console.WriteLine("You are both tall and male");
+            }
+            else
+            {
+             Console.WriteLine("You are either not both tall and male");
+            }
+            Console.ReadLine();
+        }
+    }
+}
